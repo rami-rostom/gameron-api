c7296a0476f473b35fddbacea54ce9ea0238d282
Rami <rami.rostom@proton.me>
2024-02-16 16:08:38 +0100

    :wrench: config: init server

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+// const router = require('./app/router');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
+
+// app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
