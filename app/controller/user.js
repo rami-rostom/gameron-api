aff7d1056fe3f9e412b197a03ab80890b3fb9d29
Rami <rami.rostom@proton.me>
2024-02-16 18:00:53 +0100

    :sparkles: feat: add create an user feature

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require('@prisma/client');
-
 const prisma = new PrismaClient();
+
+const slugify = require('slugify');
+const bcrypt = require('bcrypt');
 
 const controller = {
   getAllUsers: async (_req, res) => {
@@ -20,11 +22,118 @@ const controller = {
 
       const user = await prisma.user.findUnique({
         where: {
-          id: Number(id)
+          id: Number(id),
         }
       });
 
       res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
+
+  createUser: async (req, res) => {
+    try {
+      // Email must have characters before and after @, and 2 to 4 characters after the dot.
+      const EMAIL_REGEX = /^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/gm;
+
+      // Password require at least 8 characters, 1 capital letter and 1 number.
+      const PASSWORD_REGEX =
+        /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/gm;
+
+      const { email, username, password, confirmation } = req.body;
+
+      // if (!email || !username || !password) {
+      //   return res
+      //     .status(400)
+      //     .json({ error: "Missing parameter(s)." });
+      // }
+
+      if (!email) {
+        return res
+          .status(400)
+          .json({ error: "Missing email parameter(s)." });
+      }
+
+      if (!username) {
+        return res
+          .status(400)
+          .json({ error: "Missing username parameter(s)." });
+      }
+
+      if (!password) {
+        return res
+          .status(400)
+          .json({ error: "Missing password parameter(s)." });
+      }
+
+      if (password !== confirmation) {
+        return res
+          .status(400)
+          .json({ error: "Password and confirmation isn't matching." });
+      }
+
+      if (!EMAIL_REGEX.test(email)) {
+        return res
+          .status(400)
+          .json({ error: "Email is not valid." });
+      }
+
+      if (!PASSWORD_REGEX.test(password)) {
+        return res
+          .status(400)
+          .json({
+            error:
+              "Password invalid (must be at least 8 characters, include one number, one capital letter and one special character).",
+          });
+      }
+
+      // Verification for unique email
+      const emailCheck = await prisma.user.findFirst({
+        where: { email },
+      });
+
+      if (emailCheck) {
+        return res
+          .status(400)
+          .json({
+            error: "Email already used. Please try with a different email.",
+          });
+      }
+
+      // Verification for unique username
+      const usernameCheck = await prisma.user.findFirst({
+        where: { username },
+      });
+
+      if (usernameCheck) {
+        return res
+          .status(400)
+          .json({
+            error: "Username already used. Please try with a different one.",
+          });
+      }
+
+      // Slugify username
+      const usernameSlugified = slugify(username, { lower: true });
+
+      // Hash password
+      const passwordHashed = await bcrypt.hash(password, 10);
+
+      await prisma.user.create({
+        data: {
+          email,
+          username,
+          username_slug: usernameSlugified,
+          password: passwordHashed,
+          is_admin: false,
+        }
+      });
+
+      res
+        .status(201)
+        .json({ message: "User successfully created, you can now login." });
     } catch (error) {
       console.log(error);
       res.status(500).json(error.toString());
--- app/router/router.js
+++ app/router/router.js
@@ -6,5 +6,6 @@ const { userController } = require('../controller');
 // USER
 router.get('/users', userController.getAllUsers);
 router.get('/user/:id', userController.getOneUser);
+router.post('/user', userController.createUser);
 
 module.exports = router;
